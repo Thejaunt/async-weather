fbfc5cd047948cabbf78e6f5da62d6ebc48e59f5
Thejaunt <thejauntthejaunt@gmail.com>
2023-09-14 14:45:20 +0300

    updates

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ dotenv.load_dotenv()
 
 class OpenWeatherMapAPI:
     def __init__(self, city, data_collector, units=None):
-        self.city = city
+        self.city = city.get("city")
+        self.lon = city.get("lon")
+        self.lat = city.get("lat")
         self.units = units or "Metric"
         self.__api_key = os.getenv("open_weather_key")
         self.base_url = "https://api.openweathermap.org/data/2.5/weather?"
         self.collector = data_collector
 
     def get_url(self, q=None) -> str:
-        url = f"{self.base_url}appid={self.__api_key}&q={self.city}&units={self.units}&{q}"
+        url = f"{self.base_url}appid={self.__api_key}&lat={self.lat}&lon={self.lon}&units={self.units}&{q}"
         return url
 
     async def get_response(self):
@@ -37,13 +39,15 @@ class OpenWeatherMapAPI:
 
 class WeatherStackAPI:
     def __init__(self, city, data_collector):
-        self.city = city
+        self.city = city.get("city")
+        self.lon = city.get("lon")
+        self.lat = city.get("lat")
         self.__api_key = os.getenv("weatherstack_key")
         self.base_url = "http://api.weatherstack.com/current?"
         self.collector = data_collector
 
     def get_url(self, q=None):
-        url = f"{self.base_url}access_key={self.__api_key}&query={self.city}&{q}"
+        url = f"{self.base_url}access_key={self.__api_key}&query={self.lat},{self.lon}&{q}"
         return url
 
     async def get_response(self):
@@ -64,7 +68,7 @@ class Data:
     def __init__(self):
         self.data = list()
         self.failed_cities = set()
-        self.failed_cities_counter = int()
+        self.failed_request_counter = int()
 
     def append_data(self, temp):
         self.data.append(temp)
@@ -73,22 +77,22 @@ class Data:
         return self.data
 
     def record_failed_city(self, city):
-        self.failed_cities_counter += 1
+        self.failed_request_counter += 1
         self.failed_cities.add(city)
 
     def get_failed_cities(self):
         return self.failed_cities
 
 
-def calculate_average_temp(data=None, failed=0):
-    initial_cities_amount = len(data)
-    if (not data) or (initial_cities_amount == 0):
+def calculate_average_temp(data=None):
+    success_cities_amount = len(data)
+    if (not data) or (success_cities_amount == 0):
         return "Data collection failed"
     total_temp_number = float()
     for temp in data:
         total_temp_number += float(temp)
 
-    res = round(total_temp_number/(initial_cities_amount - failed), 2)
+    res = round(total_temp_number / success_cities_amount, 2)
     return res
 
 
@@ -97,26 +101,42 @@ def get_cities():
     with open("cities.json") as js:
         ct = json.load(js)
         for c in ct:
-            cities.append(c.get("city"))
+            cities.append(
+                {
+                    "city": c.get("city"),
+                    "lon": c.get("lng"),
+                    "lat": c.get("lat"),
+                }
+            )
     return cities
 
 
 async def main():
     cities = get_cities()
     collector = Data()
-    apis = [OpenWeatherMapAPI]
+    apis = [
+        OpenWeatherMapAPI,
+        WeatherStackAPI,
+    ]
     async with asyncio.TaskGroup() as tg:
         for api in apis:
             for city in cities:
                 tg.create_task(api(city, data_collector=collector).collect_temperature())
 
-    cities_failed = collector.failed_cities_counter
+    cities_failed = len(collector.failed_cities)
     cities_counted = len(collector.get_data())
-    average_temp = calculate_average_temp(collector.get_data(), failed=collector.failed_cities_counter)
-    print(f"Total cities participated - {len(cities)}")
-    print(f"Cities failed - {cities_failed}")
-    print(f"Cities counted - {cities_counted}")
-    print(f"China average temperature - {average_temp}")
+    average_temp = calculate_average_temp(collector.get_data())
+    failed_requests = collector.failed_request_counter
+    print(
+        f"""
+            APIs used - {len(apis)}
+            Total cities participated - {len(cities)}
+            Cities failed - {cities_failed}
+            Cities counted - {cities_counted}
+            Failed requests - {failed_requests}
+            China average temperature - {average_temp}* C
+        """
+    )
 
 asyncio.run(main())
 
